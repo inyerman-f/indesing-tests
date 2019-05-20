e74fdc811c9a75d4c511360d9aaefb56fbb1d2c2
inyermanFonseca <pokejodo@gmail.com>
2019-05-20 06:40:58 -0400

    update

--- php-xml-parser.php
+++ php-xml-parser.php
@@ -1,25 +1,57 @@
 <?php
 $xmlUrl = "10577-xml.xml";
-$xmlStr = file_get_contents($xmlUrl);
-$xmlStr = SimpleXMLElement($xmlStr);
+$xmlStr = simplexml_load_file($xmlUrl) or die("Error: Cannot create object");;
+//$xmlStr = SimpleXMLElement($xmlStr);
+
 ?>
 
 <html>
 <head>
-<body>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/php-parser.css">
 
-<section>
+</head>
+<body>
+<div class="document-container">
+
+<section class="standard-section product-components-item-pages-container">
     <?php
-    foreach ($xmlStr->document_container->product_components_item_pages_container as $page_container){
-        echo $page_container->product_page_title."shid";
+   foreach ($xmlStr->document_container->product_components_item_pages_container as $components_container){
+
+
+            echo '<div class="product-components-item-pages-container row">'.
+                    '<div class="product-page-title"><h3 class="page-title">'.$components_container->product_page_title.'</h3></div>';
+                    foreach ($components_container->components_items_container as $component_item_container){
+                        echo '<div class="components-items-container" style="display: flex">';
+                            foreach ($component_item_container->component as $component){
+                                $img_src = str_replace("file://lci-fileserve2/in39/DR-TP/Website-International/Steps/images/","/images/", $component->callout_group->call_out_image_container['href']);
+                                echo
+                                    '<div class="col-md-3 col-xs-6">
+                                        <div class="card callout-group">
+                                            <div class="callout-img-container col-placeholder">
+                                                <img class="card-img-top" src="'.$img_src.'">
+                                            </div>
+                                            <div class="callout-letter card-block">'.
+                                                $component->callout_group->call_out_letter.
+                                            '</div>
+                                        </div>
+                                    </div>';
+                                    }
+                            '</div>';
+                    }
+                 '</div>';
+
     }
     ?>
 
 </section>
-<section>
+
+<section class="product-components-list-pages-container">
 
 </section>
-
+</div>
 </body>
-</head>
+
 </html>
